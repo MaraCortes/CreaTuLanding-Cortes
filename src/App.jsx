bfc926a60bce61d7f2e82b4e6dbdd6eae317fb08
Mara Cortes <cortesmara25@gmail.com>
2025-11-04 16:42:04 -0300

    Primer commit de la primera preentrega

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+
+import Navbar from './Componentes/Navbar'
+import ItemListContainer from "./Componentes/ItemListContainer"
+import './App.css'
+
+function App() {
+ 
+
+  return (
+    <>
+      <Navbar/>
+      <ItemListContainer mensaje="Bienvenidos al mundo que amamos!!"/>
+    </>
+  )
+}
+
+export default App
---
+++ src/Componentes/CartWidget.jsx
@@ -0,0 +1,9 @@
+const CartWidget = () =>{
+    return(
+        <div>   
+        <img className="img-carrito" src="../carrito.png" alt="logo-carrito" />
+        </div>
+    )
+}
+
+export default CartWidget
