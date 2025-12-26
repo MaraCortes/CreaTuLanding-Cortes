9e4df26565d097f78f4d646f589e3860db088a77
Mara Cortes <cortesmara25@gmail.com>
2025-12-25 21:07:21 -0300

    Limpié el componente Item.jsx de comentarios y código no usado

--- src/Componentes/Item.jsx
+++ src/Componentes/Item.jsx
@@ -1,71 +1,3 @@
-// import {  Card } from 'react-bootstrap'
-// import { Link } from 'react-router-dom'
-
-// const Item = ({prod}) => {
-//   return (
-//      <Card style={{ width: '18rem' }}>
-//       <Card.Img variant="top" src={prod.img} />
-//       <Card.Body>
-//         <Card.Title>{prod.name}</Card.Title>
-//         <Card.Text>
-//           ${prod.price},00
-//         </Card.Text>        
-//          <Link className='btn btn-primary' to={`/item/${prod.id}`}>Ver Más</Link>
-//       </Card.Body>
-//     </Card>
-//   )
-// }
-
-// export default Item
-
-
-
-
-
-
-
-// import { Card } from 'react-bootstrap'
-// import { Link } from 'react-router-dom'
-
-// const Item = ({ prod }) => {
-//   return (
-//     <Card style={{ width: '18rem' }} className="estiloCard">
-
-//       {/* Imagen clickeable */}
-//       <Link to={`/item/${prod.id}`} className="link-img">
-//         <div className="img-wrapper">
-//           <Card.Img
-//             variant="top"
-//             src={prod.img}
-//             alt={prod.name}
-//             className="img-producto"
-//           />
-//           <div className="overlay">
-//             Ver detalle
-//           </div>
-//         </div>
-//       </Link>
-
-//       <Card.Body className="text-center">
-//         <Card.Title>{prod.name}</Card.Title>
-
-//         <Card.Text>
-//           ${prod.price},00
-//         </Card.Text>
-
-//         {/* Botón NO clickeable (solo visual) */}
-//         <button className="btn btn-outline-primary" disabled>
-//           Agregar al carrito
-//         </button>
-//       </Card.Body>
-//     </Card>
-//   )
-// }
-
-// export default Item
-
-
-
 import { Card } from 'react-bootstrap'
 import { Link } from 'react-router-dom'
 
@@ -73,7 +5,7 @@ const Item = ({ prod }) => {
   return (
     <Card style={{ width: '18rem' }} className="estiloCard">
 
-      {/* Imagen clickeable */}
+     
       <Link to={`/item/${prod.id}`} className="link-img">
         <div className="img-wrapper">
           <Card.Img
@@ -94,11 +26,7 @@ const Item = ({ prod }) => {
         <Card.Text>
           ${prod.price},00
         </Card.Text>
-
-        {/* Botón NO clickeable (solo visual)
-        <button className="btn btn-outline-primary" disabled>
-          Agregar al carrito
-        </button> */}
+        
       </Card.Body>
     </Card>
   )
